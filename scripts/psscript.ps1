f9bb431201c8b964d8741fce2d928360644f261f
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-07-02 21:28:27 +0530

    Create psscript.ps1

---
+++ scripts/psscript.ps1
@@ -0,0 +1,20 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+ 
+
+#Import Common Functions
+#. cloudlabs-common\cloudlabs-windows-functions.ps1
+$path = pwd 
+$ImportCommonFunctions = $path.Path + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $ImportCommonFunctions
+
+
+#Run Common Functions
+WindowsServerCommon
+InstallEdgeChromium
+DisableServerMgrNetworkPopup
+Disable-InternetExplorerESC
+Enable-IEFileDownload
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/srushti-714/AIAD/master/aiadmodule.psm1" -OutFile "C:\aiadmodule.psm1"
